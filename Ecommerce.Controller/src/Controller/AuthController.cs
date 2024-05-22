1a14863224b8643559011ad30da424d79d28aa69
rrtotr <toan.tran@tuni.fi>
2024-05-22 03:07:43 +0300

    edit response json from text to object

--- Ecommerce.Controller/src/Controller/AuthController.cs
+++ Ecommerce.Controller/src/Controller/AuthController.cs
@@ -7,16 +7,20 @@ namespace Ecommerce.Controller.src.Controller
 {
     [ApiController]
     [Route("api/v1/auth")]
-    public class AuthController
+    public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
-        public AuthController(IAuthService authService){
+        public AuthController(IAuthService authService)
+        {
             _authService = authService;
         }
 
         [HttpPost("login")]
-        public async Task<string> LoginAsync(UserCredential userCredential){
-            return await _authService.LoginAsync(userCredential);
+        public async Task<IActionResult> LoginAsync(UserCredential userCredential)
+        {
+            var accessToken = await _authService.LoginAsync(userCredential);
+            var response = new { access_token = accessToken };
+            return Ok(response);
         }
     }
 }
